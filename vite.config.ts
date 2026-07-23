9a4ae322d5d0edda8e386d2ac56cd2fc86926e76
Kiguti Fenix <kigutifenix@gmail.com>
2026-07-23 11:17:55 -0300

    fix: add index.html root entry point for vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
-    tsconfigPaths(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 8080,
     host: true,
