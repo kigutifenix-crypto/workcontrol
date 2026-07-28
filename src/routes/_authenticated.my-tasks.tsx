2233d540113c1f0b1a58a6afe174d65669e26e3d
Kiguti Fenix <kigutifenix@gmail.com>
2026-07-28 09:40:42 -0300

    fix: import useEffect to resolve typescript compiler errors

--- src/routes/_authenticated.my-tasks.tsx
+++ src/routes/_authenticated.my-tasks.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { AppShell } from "@/components/app-shell";
 import { supabase } from "@/integrations/supabase/client";
 import { useAuth } from "@/lib/auth";
